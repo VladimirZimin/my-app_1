d1597d2c56c70b3bab33b2da29bd24919d77baa9
VladimirZimin <zimin.frontend@gmail.com>
2023-10-12 21:44:13 +0300

    qwe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Section from './components/Section';
 import paintings from './paintings.json';
 
 export default function App() {
+  const arrs = [[1], [[2], [3]], [[[4]]]];
+
+  const fx = arrs => arrs.flat(3);
+
+  console.log(fx(arrs));
+
   return (
     <div>
       <Section title="Top week">
